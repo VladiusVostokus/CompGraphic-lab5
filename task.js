932ccd2fe8988b454225bcf033d66aa1d64767d2
VladiusVostokus <kovalenkov222@gmail.com>
2024-12-03 12:13:57 +0200

    Multiply X and Y matrixes in JS code instead of vertex code

--- task.js
+++ task.js
@@ -3,16 +3,16 @@
 const vsSource = `#version 300 es
 in vec3 aPosition;
 in vec3 aNormal;
-uniform mat4 uProjectionMatrix_Y;
+uniform mat4 uTransformMatrix;
 uniform mat4 uProjectionMatrix_Z;
 uniform mat4 uPerspectiveMatrix;
 uniform mat4 uModelViewMatrix;
 out vec4 vNormal;
 
 void main() {
-    vec4 modeledNormal = uPerspectiveMatrix * uModelViewMatrix * uProjectionMatrix_Y * uProjectionMatrix_Z * vec4(aNormal, 1.0);
+    vec4 modeledNormal = uPerspectiveMatrix * uModelViewMatrix * uTransformMatrix * vec4(aNormal, 1.0);
     vec4 normalizedNormal = normalize(modeledNormal);
-    gl_Position = uPerspectiveMatrix * uModelViewMatrix * uProjectionMatrix_Y * uProjectionMatrix_Z * vec4(aPosition, 1.0);
+    gl_Position = uPerspectiveMatrix * uModelViewMatrix * uTransformMatrix * vec4(aPosition, 1.0);
     vNormal = normalizedNormal;
 }`;
 
@@ -68,8 +68,7 @@ function main() {
 
     const aPosition = gl.getAttribLocation(program, 'aPosition');
     const aNormal = gl.getAttribLocation(program, 'aNormal');
-    const uProjectionMatrix_Y = gl.getUniformLocation(program,'uProjectionMatrix_Y');
-    const uProjectionMatrix_Z = gl.getUniformLocation(program,'uProjectionMatrix_Z');
+    const uTransformMatrix = gl.getUniformLocation(program,'uTransformMatrix');
     const uPerspectiveMatrix = gl.getUniformLocation(program,"uPerspectiveMatrix");
     const uModelViewMatrix = gl.getUniformLocation(program, "uModelViewMatrix");
     const uAmbientColor = gl.getUniformLocation(program,'uAmbientColor');
@@ -175,6 +174,37 @@ function main() {
             m[3], m[7], m[11], m[15],
         ];
     }
+
+    const multiply = (a, b) => {
+        var a00 = a[0 * 4 + 0], a01 = a[0 * 4 + 1], a02 = a[0 * 4 + 2], a03 = a[0 * 4 + 3];
+        var a10 = a[1 * 4 + 0], a11 = a[1 * 4 + 1], a12 = a[1 * 4 + 2], a13 = a[1 * 4 + 3];
+        var a20 = a[2 * 4 + 0], a21 = a[2 * 4 + 1], a22 = a[2 * 4 + 2], a23 = a[2 * 4 + 3];
+        var a30 = a[3 * 4 + 0], a31 = a[3 * 4 + 1], a32 = a[3 * 4 + 2], a33 = a[3 * 4 + 3];
+        var b00 = b[0 * 4 + 0], b01 = b[0 * 4 + 1], b02 = b[0 * 4 + 2], b03 = b[0 * 4 + 3];
+        var b10 = b[1 * 4 + 0], b11 = b[1 * 4 + 1], b12 = b[1 * 4 + 2], b13 = b[1 * 4 + 3];
+        var b20 = b[2 * 4 + 0], b21 = b[2 * 4 + 1], b22 = b[2 * 4 + 2], b23 = b[2 * 4 + 3];
+        var b30 = b[3 * 4 + 0], b31 = b[3 * 4 + 1], b32 = b[3 * 4 + 2], b33 = b[3 * 4 + 3];
+      
+        return [
+          b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
+          b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
+          b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
+          b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
+          b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
+          b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
+          b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
+          b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
+          b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
+          b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
+          b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
+          b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
+          b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
+          b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
+          b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
+          b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
+        ];
+    };
+
     let angle = 0.0;
 
     const draw = () => {
@@ -195,15 +225,16 @@ function main() {
             0,0,0,1,
         ];
 
-        const projectionMatrix_Z = new Float32Array([
+        const projectionMatrix_X = new Float32Array([
             1,0,0,0,
             0,cos,sin,0,
             0,-sin,cos,0,
             0,0,0,1,
         ]);
 
-        gl.uniformMatrix4fv(uProjectionMatrix_Y, false, projectionMatrix_Y);
-        gl.uniformMatrix4fv(uProjectionMatrix_Z, false, projectionMatrix_Z);
+        const transformMatrix = multiply(projectionMatrix_Y, projectionMatrix_X)
+
+        gl.uniformMatrix4fv(uTransformMatrix, false,  transformMatrix);
         gl.uniformMatrix4fv(uPerspectiveMatrix, false, perspectiveMatrix);
         gl.uniformMatrix4fv(uModelViewMatrix, false, modelViewMatrix)
         gl.drawArrays(gl.TRIANGLES, 0, 36);
